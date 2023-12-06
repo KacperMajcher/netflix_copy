e03979cc8e818e717c1e65eb03d31c3bf2f0489a
kacpermajcher <kacpermajcher13@gmail.com>
2023-12-06 12:46:25 +0100

    Create more likely content for details page

--- lib/features/details/widgets/more_likely.dart
+++ lib/features/details/widgets/more_likely.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_copy/shared_widgets/cards/movie_card.dart';
 
 class MoreLikely extends StatelessWidget {
   const MoreLikely({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      color: Colors.blue,
-      child: const Center(
-        child: Text("1"),
+    return const SizedBox(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: SizedBox()),
+              MovieCard(
+                cover: 'assets/covers/lacasadepapel_cover.jpg',
+              ),
+              Expanded(child: SizedBox()),
+              MovieCard(
+                cover: 'assets/covers/caos_cover.jpg',
+              ),
+              Expanded(child: SizedBox()),
+              MovieCard(
+                cover: 'assets/covers/13reasonswhy_cover.jpg',
+              ),
+              Expanded(child: SizedBox()),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(child: SizedBox()),
+              MovieCard(
+                cover: 'assets/covers/breakingbad_cover.jpg',
+              ),
+              Expanded(child: SizedBox()),
+              MovieCard(
+                cover: 'assets/covers/prisonbreak_cover.jpg',
+              ),
+              Expanded(child: SizedBox()),
+              MovieCard(
+                cover: 'assets/covers/you_cover.jpg',
+              ),
+              Expanded(child: SizedBox()),
+            ],
+          ),
+        ],
       ),
     );
   }
