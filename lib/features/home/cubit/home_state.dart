8e49047261a6bfcc5d7cad1b6f2a89b6523ba30a
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-19 11:46:40 +0100

    Implement BLoC

---
+++ lib/features/home/cubit/home_state.dart
@@ -0,0 +1,9 @@
+part of 'home_cubit.dart';
+
+class HomeState {
+  HomeState();
+  final String page = 'assets/covers/anne_cover.jpg';
+  final String title = 'Anne';
+  final String description =
+      'Two sisters fight on opposite sides in the war between the cities of Piltover and Zaun, where magical technologies and conflicting beliefs clash.';
+}
--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:netflix_copy/features/details/pages/details_page.dart';
+import 'package:netflix_copy/features/home/cubit/home_cubit.dart';
 import 'package:netflix_copy/shared_widgets/categories.dart';
 import 'package:netflix_copy/shared_widgets/categories/category.dart';
 import 'package:netflix_copy/shared_widgets/icons/cast_icon_button.dart';
@@ -13,94 +15,103 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color(0xFF535051),
-              Color(0xFF341010),
-            ],
-          ),
-        ),
-        child: Scaffold(
-          bottomNavigationBar: const NavigationBarWidget(),
-          backgroundColor: Colors.transparent,
-          appBar: appBar(),
-          body: ListView(
-            children: [
-              Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-                  child: Stack(
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const DetailsPage()));
-                        },
-                        child: Container(
-                          height: 466,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/covers/lucyfer_cover.jpg'),
-                                fit: BoxFit.cover,
-                                alignment: Alignment.topCenter),
-                            color: Colors.grey,
-                            borderRadius:
-                                BorderRadius.all(Radius.elliptical(10, 10)),
-                          ),
-                        ),
-                      ),
-                      const Column(
-                        children: [
-                          SizedBox(height: 412),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(width: 12),
-                              Expanded(
-                                  child: PlayButton(
-                                backgroundColor: Colors.white,
-                                textColor: Colors.black,
-                              )),
-                              SizedBox(width: 12),
-                              Expanded(
-                                  child: MyListButton(
-                                textColor: Colors.white,
-                                backgroundColor: Color(0xFF32302D),
-                              )),
-                              SizedBox(width: 12),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  )),
-              Category(
-                  title: 'My list', category: category1(), showViewAll: true),
-              Category(
-                  title: 'European series',
-                  category: category2(),
-                  showViewAll: false),
-              Category(
-                  title: 'Popular now',
-                  category: category3(),
-                  showViewAll: false),
-              Category(
-                  title: 'Netflix exclusives',
-                  category: category4(),
-                  showViewAll: false),
-              const SizedBox(height: 30),
-            ],
-          ),
-        ),
+    return BlocProvider(
+      create: (context) => HomeCubit(),
+      child: BlocBuilder<HomeCubit, HomeState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xFF535051),
+                    Color(0xFF341010),
+                  ],
+                ),
+              ),
+              child: Scaffold(
+                bottomNavigationBar: const NavigationBarWidget(),
+                backgroundColor: Colors.transparent,
+                appBar: appBar(),
+                body: ListView(
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 20),
+                        child: Stack(
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const DetailsPage()));
+                              },
+                              child: Container(
+                                height: 466,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(state.page),
+                                      fit: BoxFit.cover,
+                                      alignment: Alignment.topCenter),
+                                  color: Colors.grey,
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.elliptical(10, 10)),
+                                ),
+                              ),
+                            ),
+                            const Column(
+                              children: [
+                                SizedBox(height: 412),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(width: 12),
+                                    Expanded(
+                                        child: PlayButton(
+                                      backgroundColor: Colors.white,
+                                      textColor: Colors.black,
+                                    )),
+                                    SizedBox(width: 12),
+                                    Expanded(
+                                        child: MyListButton(
+                                      textColor: Colors.white,
+                                      backgroundColor: Color(0xFF32302D),
+                                    )),
+                                    SizedBox(width: 12),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        )),
+                    Category(
+                        title: 'My list',
+                        category: category1(),
+                        showViewAll: true),
+                    Category(
+                        title: 'European series',
+                        category: category2(),
+                        showViewAll: false),
+                    Category(
+                        title: 'Popular now',
+                        category: category3(),
+                        showViewAll: false),
+                    Category(
+                        title: 'Netflix exclusives',
+                        category: category4(),
+                        showViewAll: false),
+                    const SizedBox(height: 30),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
