7ec667d9665241a681e384ee4b09227a257cf3bb
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-22 12:59:50 +0100

    Use ENV files to hide API data

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,3 +1,5 @@
+import 'package:netflix_copy/env/env.dart';
+
 enum Flavor {
   development,
   production,
@@ -5,6 +7,44 @@ enum Flavor {
 
 class Config {
   static Flavor appFlavor = Flavor.development;
+
+  static String url = '${Config.baseUrl}${Config.urlSettings}${Config.apiKey}';
+
+  static String get baseUrl {
+    switch (appFlavor) {
+      case Flavor.development:
+        return Env.LINK;
+      case Flavor.production:
+        return Env.LINK;
+    }
+  }
+
+  static String get urlSettings {
+    switch (appFlavor) {
+      case Flavor.development:
+        return Env.SET;
+      case Flavor.production:
+        return Env.SET;
+    }
+  }
+
+  static String get apiKey {
+    switch (appFlavor) {
+      case Flavor.development:
+        return Env.KEY1;
+      case Flavor.production:
+        return Env.KEY1;
+    }
+  }
+
+  static String get imageReader {
+    switch (appFlavor) {
+      case Flavor.development:
+        return Env.LINK2;
+      case Flavor.production:
+        return Env.LINK2;
+    }
+  }
 
   static bool get debugShowCheckedModeBanner {
     switch (appFlavor) {
