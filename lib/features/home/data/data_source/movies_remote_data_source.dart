97df4ac25f4767e7f9c7d3b5e2d2f1619b7002ea
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-22 13:54:01 +0100

    Add Dio handling errors

--- lib/features/home/data/data_source/movies_remote_data_source.dart
+++ lib/features/home/data/data_source/movies_remote_data_source.dart
@@ -3,7 +3,11 @@ import 'package:netflix_copy/core/config.dart';
 
 class MoviesRemoteDioDataSource {
   Future<Map<String, dynamic>?> getMoviesData() async {
-    final response = await Dio().get<Map<String, dynamic>>(Config.url);
-    return response.data;
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(Config.url);
+      return response.data;
+    } on DioException catch (error) {
+      throw Exception(error.response?.data['error'] ?? 'Unknown error');
+    }
   }
 }
