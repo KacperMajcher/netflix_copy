1d7771e2aa1ec453e73f1cc91d6dacb6afa46429
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-26 08:33:48 +0100

    Create DTO for Api

--- lib/features/home/data/repository/movies_repository.dart
+++ lib/features/home/data/repository/movies_repository.dart
@@ -1,19 +1,25 @@
 import 'package:netflix_copy/features/home/data/data_source/movies_remote_data_source.dart';
+import 'package:netflix_copy/features/home/data/entities/api_dto.dart';
 import 'package:netflix_copy/features/home/data/model/movie_model.dart';
 
 class MoviesRepository {
   MoviesRepository({required this.remoteDataSource});
 
-  final MoviesRemoteDioDataSource remoteDataSource;
+  final MoviesRemoteRetrofitDataSource remoteDataSource;
 
   Future<List<MovieModel>> getMoviesData() async {
-    final json = await remoteDataSource.getMoviesData();
-    if (json == null) {
-      return [];
-    }
-    //converter
-    return (json['results'] as List)
-        .map((item) => MovieModel.fromJson(item as Map<String, dynamic>))
-        .toList();
+    final MovieResponseDto response = (await remoteDataSource.getMoviesData());
+
+    final List<MovieModel> movies = response.results.map((movieDto) {
+      return MovieModel(
+        id: movieDto.id,
+        cover: movieDto.backdropPath,
+        title: movieDto.originalTitle,
+        release: movieDto.releaseDate,
+        overview: movieDto.overview,
+      );
+    }).toList();
+
+    return movies;
   }
 }
