07a1e5c0d6024702e9a0480aba5872924c9b0222
Kacper Majcher <kacpermajcher13@gmail.com>
2024-05-25 17:39:35 +0200

    Add a background under the covers on the HomePage

--- lib/shared_widgets/categories/category.dart
+++ lib/shared_widgets/categories/category.dart
@@ -46,9 +46,20 @@ class Category extends StatelessWidget {
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Image.network(
-                  covers[index],
-                  fit: BoxFit.cover,
+                child: Container(
+                  width: 100,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: const Color(0xFF736D6D),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.network(
+                      covers[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
               );
             },
