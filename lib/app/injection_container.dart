a44979a860e71f1881557405443993d8c8fe22cd
Kacper Majcher <kacpermajcher13@gmail.com>
2024-06-16 11:34:22 +0200

    Add DetailsCubit to injection container

--- lib/app/injection_container.dart
+++ lib/app/injection_container.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:netflix_copy/app/services/api_keys.dart';
+import 'package:netflix_copy/features/details/pages/cubit/details_cubit.dart';
 import 'package:netflix_copy/features/home/cubit/home_cubit.dart';
 import 'package:netflix_copy/features/home/data/data_source/movies_remote_data_source.dart';
 import 'package:netflix_copy/features/home/data/data_source/tv_series_remote_data_source.dart';
@@ -12,6 +13,7 @@ final getIt = GetIt.instance;
 void configureDependencies() {
   // Bloc (cubit)
   getIt.registerFactory(() => HomeCubit(getIt(), getIt()));
+  getIt.registerFactory(() => DetailsCubit(getIt()));
 
   // Repository
   getIt.registerFactory(() => MoviesRepository(remoteDataSource: getIt()));
